b81e949de76445790a1ee4504c2df7607843e999
ctrljean <ctrljean@gmail.com>
2023-05-17 16:35:46 -0500

    scaffolding in 90%

---
+++ packages/employees/src/EmployeesServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Lifespikes\Employees;
+
+use Illuminate\Support\ServiceProvider;
+
+class EmployeesServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
